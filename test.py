1d7e0572b8ca24ef92f39296b873a53890e9fa7c
dydokamil <dydokamil@gmail.com>
2016-12-26 13:34:57 +0100

    Save predictions to an array

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 # coding=utf-8
-from tools import TEST_DIR, ModelController
-import numpy as np
+from tools import ModelController
+import pandas as pd
 
 model_controller = ModelController()
 model = model_controller.get_model()
+predictions = []
 
 while True:
     try:
         test = model_controller.get_test_data()
-        break
+        prediction = model.predict(test, verbose=1)
+        predictions.append(prediction)
     except StopIteration:
         break
 
-predictions = model.predict(test)
-print(predictions)
